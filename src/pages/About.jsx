bed0d47bb56fe8f970f49d1f74c27a06449c9eb3
miranijay <miranijay188@gmail.com>
2025-06-10 00:44:57 +0530

    About update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React from 'react';
+import countryFacts from '../api/countryFacts.json';
 
 const About = () => {
+  
   return (
-    <h1>
-        About Page
-    </h1>
+    <section className='section-about container'>
+      <h2 className="container-title">
+        Here are the Interesting Facts
+        <br />
+        we're proud of
+      </h2>
+
+      <div className="gradient-cards">
+
+        {
+          countryFacts.map((country) => {
+            const {id, countryName, capital, population, interestingFact} = country;
+
+            return (
+              <div className="card" key={id}>
+                <div className="container-card bg-blue-box">
+                  <p className="card-title"> {countryName} </p>
+                  <p>
+                    <span className="card-description"> Capital: </span>
+                    {capital}
+                  </p>
+                  <p>
+                    <span className="card-description"> Population: </span>
+                    {population}
+                  </p>
+                  <p>
+                    <span className="card-description"> InterestingFacts: </span>
+                    {interestingFact}
+                  </p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
