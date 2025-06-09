52865f9609e243b465c3da7c38ad84bc0d230456
miranijay <miranijay188@gmail.com>
2025-06-10 02:22:14 +0530

    Footer and Contact update

--- src/Components/UI/Footer.jsx
+++ src/Components/UI/Footer.jsx
@@ -1,10 +1,88 @@
 import React from 'react'
+import footerData from '../../api/footerApi.json';
+import { IoCallSharp } from "react-icons/io5";
+import { MdPlace } from "react-icons/md";
+import { TbMailPlus } from "react-icons/tb";
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
+
+  const footerIcon = {
+    MdPlace: <MdPlace />,
+    IoCallSharp: <IoCallSharp />,
+    TbMailPlus: <TbMailPlus />
+  }
+
   return (
-    <h1>
-        Footer
-    </h1>
+    <>
+      <footer className='footer-section'>
+        <div className='container grid grid-three-cols'>
+          {
+            footerData.map((currData, index) => {
+                const {icon, title, details} = currData;
+
+                return(
+                  <div className="footer-contact" key={index}>
+                    <div className="icon">
+                      {footerIcon[icon]}
+                    </div>
+                    <div className="footer-contact-text">
+                      <p>{title}</p>
+                      <p>{details}</p>
+                    </div>
+                  </div>
+                )
+            })
+          }
+        </div>
+
+        <div className="copyright-area">
+          <div className="container">
+            <div className="grid grid-two-cols">
+              <div className="copyright-text">
+                <p>
+                  Copyright &copy; 2025, All Right Reserved 
+                  <NavLink to='https://github.com/miranijay' target='_blank'>
+                    JayMirani
+                  </NavLink> 
+                </p>
+              </div>
+
+              <div className="footer-menu">
+                <ul>
+                  <li>
+                    <NavLink to='/'>
+                      Home
+                    </NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to='https://github.com/miranijay' target='_blank'>
+                      Social
+                    </NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink 
+                      to='https://github.com/miranijay/React19-Website-WorldAtlas'
+                      target='_blank'
+                    >
+                      Source-Code
+                    </NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to='/contact'> 
+                      Contact
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 
 const Contact = () => {
+
+  const handleFormSubmit = (formData) => {
+      const formInputData = Object.fromEntries(formData.entries());
+      console.log(formInputData);
+  }
+
   return (
-    <h1>
-        Contact Page
-    </h1>
+    <>
+      <section className="section-contact">
+        <h2 className="container-title">
+          Contact Us
+        </h2>
+
+        <div className="container contact-wrapper">
+
+          <form action={handleFormSubmit}>
+            <input
+              type="text"
+              required
+              autoComplete='off'
+              placeholder='Enter Your Name'
+              name='username'
+              className='form-control'
+            />
+
+            <input
+              type="email"
+              required
+              autoComplete='off'
+              placeholder='Enter Your Email'
+              name='email'
+              className='form-control'
+            />
+
+            <textarea
+              name="message"
+              className='form-control'
+              required
+              rows={10}
+              autoComplete='off'
+              placeholder='Enter Your Message'
+            ></textarea>
+
+            <button type='submit' value='send'>
+              Send
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
   )
 }
 
