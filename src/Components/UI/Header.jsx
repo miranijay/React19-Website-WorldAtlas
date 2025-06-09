e26ba4528f849b34f514b324ee51c395bfce4866
miranijay <miranijay188@gmail.com>
2025-06-09 16:54:31 +0530

    Hero-section update

--- src/Components/UI/Header.jsx
+++ src/Components/UI/Header.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <h1>
-        Header
-    </h1>
+    <>
+        <header>
+            <div className="container">
+                <div className="grid navbar-grid">
+                    <div className='logo'>
+                        <NavLink to='/'>
+                            <h1> WorldAtlas </h1>
+                        </NavLink>
+                    </div>
+
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink to='/'>
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/about'>
+                                    About
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/country'>
+                                    Country
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/contact'>
+                                    Contact
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    </>
   )
 }
 
